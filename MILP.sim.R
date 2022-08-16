85811c5f8e19970d95d61e628caf2f781234b3f0
John Cameron <gnyku@WUST045985.bayer.cnb>
2022-08-16 10:11:35 -0700

    update 2

--- MILP.sim.R
+++ MILP.sim.R
@@ -1,5 +1,3 @@
-#TEST
-
 library(lpSolve)
 noloci=10
 noinds=100
@@ -18,7 +16,7 @@ mem=geno*effects
 
 #1 Data a
 #Selecting mate-pair constraint
-#Output is dataijk 
+#dataijk ≤ noinds*noloci*Sk
 datacons<-function(noloci,noinds){
 	one<-seq(1,noinds*noloci*2,noloci*2)
 	two<-seq(noloci*2,noinds*noloci*2,noloci*2)
@@ -32,23 +30,36 @@ datacons<-function(noloci,noinds){
 		return(ret)
 	}
 
+dc<-datacons(noloci,noinds)
+
 #2 X matrix
+#Each value in X is the sum along 3rd dimension of data matrix
 xmat=function(noloci,noinds){
 		#Data variable matrix
+		#LHS
 	it=array(0,dim=c(noloci*2,noloci*2*noinds))
 		for(i in 1:(noloci*2)){
 			it[i,seq(i,noloci*2*noinds,20)]=1
 		}
-		#X Matrix variable
+		#RHS
 			x=diag(20)
-			itx=list(x,it)
+			itx=list(it,x)
 			return(itx)
 	}
 
+xm<-xmat(noloci,noinds)
+
 #3 Transition matrix
-tmat=function(noloci){
-	iter=(nrow(tm)/4)-4
-	tm<-array(0,dim=c(noloci*16,noloci*16))
+#sumj(Tijk)=1
+#TM:noloci*16/4 - 4
+#TM2:16*noloci,noloci
+#T11k,T21k,T31k,T41k,T22k,T12k,T32k,T42k,T33k,T13k,T23k,T43k,T44k,T14k,T24k,T34k
+
+#Need to modify new linkage group constraints in tm2
+#1) T111 + T221 + T331 + T441 = 1
+tmat=function(noloci,lgend){
+	iter=(noloci*16/4)-4
+	tm<-array(0,dim=c(iter,noloci*16))
 	ind1=1:4
 	ind2=17:20
 	for(i in 1:iter){
@@ -63,9 +74,18 @@ tmat=function(noloci){
 		b=seq(16,ncol(tm),16)
 		
 		for(i in 1:(noloci)){
+			if(i%in%lgend){
+				tm2[i,c(1,5,9,13)]=1
+				}else{
 			tm2[i,a[i]:b[i]]=1
 		}
-
-	return(rbind(tm,tm2))
+	}
+	return(list(tm,tm2))
 }
+
+tm=tmat(noloci,lgend)
+#Markers at end of linkage group
+lgend<-1
+
+
 
